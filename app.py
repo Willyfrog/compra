4d2483bf8e79381c404872b30cfdda07f50e316b
Guillermo Vaya <guivaya@gmail.com>
2012-11-04 12:20:47 +0100

    añadido metodo para gestion de item

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def logout():
 
 @route('/static/<file:path>')
 def static(file):
-    #TODO: prevent accessing .. or any other thingino
+    #TODO: prevent accessing .. or any other thing
     return static_file(file, root='./static/')
 
 @route('/404')
@@ -42,6 +42,32 @@ def error_method():
 
 
 # Rutas dinamicas del server
+
+@route('/<module>/<item:int>/<action>')
+def run_action_on_item(module,item,action):
+    try:
+        m = import_module(module)
+    except ImportError:
+        redirect('/404')
+    action_call = module + '_' + action
+    print "DEBUG: tenemos el modulo, ahora llamaremos a %s" % action_call
+    try:
+        a = getattr(m, action_call)
+    except AttributeError, e:  # diferencia entre fallo por modulo o accion
+        print "ERROR: module %s doesn't have a %s function" % (module, action_call)
+        print "ERROR: %s" % e
+        redirect('/400')
+    #TODO: añadir autenticacion
+    metodo = request.method
+    get_p = request.query
+    post_p = request.forms
+    # por simplificar, pasamos siempre 4 parametros:
+    #   - metodo: GET,POST,PUT,DELETE
+    #   - item: identificador del elemento
+    #   - get_p: parametros de la query (?cosa=otracosa)
+    #   - post_p: parametros de post-data
+    # por lo que cada funcion debera comprobar que tiene todo
+    return a(metodo, item, get_p, post_p)
 
 @route('/<module>/<action>')
 def run_action(module,action):
