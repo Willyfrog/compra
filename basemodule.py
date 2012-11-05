f6066e9d2ae5d402a63d3c62586dbaa069428924
Guillermo Vaya <guivaya@gmail.com>
2012-11-05 01:25:52 +0100

    Añadido modulo base de donde heredar otras abstracciones del modelo de datos de la api

---
+++ basemodule.py
@@ -0,0 +1,50 @@
+class Document(object):
+    """
+    Establece las funciones basicas para la abstraccion del documento
+    de mongodb y el json usado para la api
+    """
+
+    def __init__(self, document):
+        """
+        guarda el documento para su uso posterior
+
+        Arguments:
+        - `document`:
+        """
+        self._document = document
+
+    def __getattr__(self, name):
+        """
+        si no es un atributo que ya tuvieramos definido
+        buscamos en _document para devolverlo
+        Esto nos permite acceder a los atributos del documento
+        """
+        if name in self._document:
+            return self._document['name']
+        else:
+            raise AttributeError("%r object has no attribute %r" %
+                                 (type(self)._name, name))
+
+    def show(showonly=None):
+        """
+        Muestra el objeto completo si no se le pasan parametros
+        o solo los seleccionados si showonly es dtto de None
+
+        Arguments:
+        - `showonly`:
+        """
+        res = dict()
+        for key in self._document.keys():
+            if key!="_id":  # nos saltamos esta que no le interesa al usuario
+                if showonly is None or getattr(showonly,key,False):
+                    res[key] = self._document[key]
+        if showonly is None or getattr(showonly,'_methods',False):
+            res['_methods'] = dict()
+
+
+
+    def __unicode__():
+        """
+        Crea la representacion del objeto de cara a mostrarlo
+        """
+        self.show()
--- listas.py
+++ listas.py
@@ -1,4 +1,5 @@
 import pymongo
+from basemodule import Document
 
 def listas_listas():
     return "funciona!"
@@ -6,4 +7,14 @@ def listas_listas():
 class Listas(object):
     '''gestor de listas'''
     def __init__(self, db):
+        '''recibe db como una conexion a la base de datos para usarla'''
         self.db = db
+
+        def list():
+            """
+            """
+
+class Lista(Document):
+    """
+    Abstracion entre el documento de mongo y el json de la web
+    """
